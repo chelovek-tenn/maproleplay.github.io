3ea6be323ec1e5149139e64766ff107e9a4931f6
l-a-roleplay <121698621+l-a-roleplay@users.noreply.github.com>
2023-01-03 02:24:04 +0100

    Update markers_num.js

--- js/markers_num.js
+++ js/markers_num.js
@@ -1,4 +1,4 @@
-https://i.postimg.cc/4d2SmHxT/image.png  L.marker([-5662, 3401], {icon: cardIcon4}).addTo(map)
+L.marker([-5662, 3401], {icon: cardIcon4}).addTo(map)
     .bindPopup("<h2>Gouvernement</h2><img class='logo' src='https://i.postimg.cc/FKFth8y8/image-1.png'><p><a href='https://i.postimg.cc/Xv1P4J7B/IMG-20230102-024358.jpg' data-lightbox='image-4' data-title='Gouvernement'>Ouvrir le visuel du lieu</a><h3>Siège du gouvernement</h3></p>");
   L.marker([-5744.6, 3472], {icon: cardIcon3}).addTo(map)
     .bindPopup("<h2>Hôpital</h2><img class='logo' src='https://i.postimg.cc/FsZRsgyd/image.png'><p><a href='https://i.postimg.cc/WpSLd9yv/neblife-fa8551fbc3a20dd3fd5c57cef430459e.png' data-lightbox='image-4' data-title='Hôpital'>Ouvrir le visuel du lieu</a><h3>Hôpital Mount Zonah</h3></p>");
