1fe33dc238e151e2691e920f3ef0bed5cef92f47
l-a-roleplay <121698621+l-a-roleplay@users.noreply.github.com>
2023-01-08 02:27:55 +0100

    Update markersbis.js

--- js/markersbis.js
+++ js/markersbis.js
@@ -1,17 +1,17 @@
 L.marker([-6120, 4258], {icon: cardIcon35}).addTo(map)
-    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/wxJppdDf/gentrepot.png' data-lightbox='image-4' data-title='Entrepôt Darnel Bros'>Ouvrir le visuel du lieu</a><h3>Entrepôt East LS</h3></p>");
+    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/0QLg0zf9/gentrepot.png' data-lightbox='image-4' data-title='Entrepôt Darnel Bros'>Ouvrir le visuel du lieu</a><h3>Entrepôt East LS</h3></p>");
 L.marker([-7188, 4420], {icon: cardIcon35}).addTo(map)
-    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/wxJppdDf/gentrepot.png' data-lightbox='image-4' data-title='Entrepôt Cypress'>Ouvrir le visuel du lieu</a><h3>Entrepôt East LS</h3></p>");
+    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/0QLg0zf9/gentrepot.png' data-lightbox='image-4' data-title='Entrepôt Cypress'>Ouvrir le visuel du lieu</a><h3>Entrepôt East LS</h3></p>");
 L.marker([-6990, 3815.2], {icon: cardIcon35}).addTo(map)
-    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/wxJppdDf/gentrepot.png' data-lightbox='image-4' data-title='Entrepôt Walker & Sons'>Ouvrir le visuel du lieu</a><h3>Entrepôt South LS</h3></p>");
+    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/0QLg0zf9/gentrepot.png' data-lightbox='image-4' data-title='Entrepôt Walker & Sons'>Ouvrir le visuel du lieu</a><h3>Entrepôt South LS</h3></p>");
 L.marker([-7328, 3176], {icon: cardIcon35}).addTo(map)
-    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/wxJppdDf/gentrepot.png' data-lightbox='image-4' data-title='Entrepôt Bilgeco'>Ouvrir le visuel du lieu</a><h3>Entrepôt South LS</h3></p>");
+    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/0QLg0zf9/gentrepot.png' data-lightbox='image-4' data-title='Entrepôt Bilgeco'>Ouvrir le visuel du lieu</a><h3>Entrepôt South LS</h3></p>");
 L.marker([-6555, 4389], {icon: cardIcon35}).addTo(map)
-    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/wxJppdDf/gentrepot.png' data-lightbox='image-4' data-title='Entrepôt Logistics Depot'>Ouvrir le visuel du lieu</a><h3>Entrepôt East LS</h3></p>");
+    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/0QLg0zf9/gentrepot.png' data-lightbox='image-4' data-title='Entrepôt Logistics Depot'>Ouvrir le visuel du lieu</a><h3>Entrepôt East LS</h3></p>");
 L.marker([-6951, 3041], {icon: cardIcon35}).addTo(map)
-    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/wxJppdDf/gentrepot.png' data-lightbox='image-4' data-title='Entrepôt Xero Gas Factory'>Ouvrir le visuel du lieu</a><h3>Entrepôt South LS</h3></p>");
+    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/0QLg0zf9/gentrepot.png' data-lightbox='image-4' data-title='Entrepôt Xero Gas Factory'>Ouvrir le visuel du lieu</a><h3>Entrepôt South LS</h3></p>");
  L.marker([-5396, 3579.2], {icon: cardIcon35}).addTo(map)
-    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/wxJppdDf/gentrepot.png' data-lightbox='image-4' data-title='Entrepôt West Vinewood Backlot'>Ouvrir le visuel du lieu</a><h3>Entrepôt North LS</h3></p>");
+    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/0QLg0zf9/gentrepot.png' data-lightbox='image-4' data-title='Entrepôt West Vinewood Backlot'>Ouvrir le visuel du lieu</a><h3>Entrepôt North LS</h3></p>");
 L.marker([-6955, 4422], {icon: cardIcon35}).addTo(map)
-    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/wxJppdDf/gentrepot.png' data-lightbox='image-4' data-title='Entrepôt Wholesale Furniture'>Ouvrir le visuel du lieu</a><h3>Entrepôt East LS</h3></p>");
+    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/0QLg0zf9/gentrepot.png' data-lightbox='image-4' data-title='Entrepôt Wholesale Furniture'>Ouvrir le visuel du lieu</a><h3>Entrepôt East LS</h3></p>");
  
