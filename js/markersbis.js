90cf88bb6071f133d168de76a85747530e5e5ed0
l-a-roleplay <121698621+l-a-roleplay@users.noreply.github.com>
2023-01-08 04:53:49 +0100

    Update markersbis.js

--- js/markersbis.js
+++ js/markersbis.js
@@ -1,48 +1,48 @@
 L.marker([-6120, 4258], {icon: cardIcon35}).addTo(map)
-    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/0QLg0zf9/gentrepot.png'><p><a href='https://imgur.com/JOurhRf.png' data-lightbox='image-4' data-title='Entrepôt Darnel Bros'>Ouvrir le visuel du lieu</a><h3>Entrepôt East LS</h3></p>");
+    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/0QLg0zf9/gentrepot.png'><p><a href='https://i.postimg.cc/3wkdpmvn/IMG-20230108-043112.jpg' data-lightbox='image-4' data-title='Entrepôt Darnel Bros'>Ouvrir le visuel du lieu</a><h3>Entrepôt East LS</h3></p>");
 L.marker([-7188, 4420], {icon: cardIcon35}).addTo(map)
-    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/0QLg0zf9/gentrepot.png'><p><a href='https://imgur.com/JOurhRf.png' data-lightbox='image-4' data-title='Entrepôt Cypress'>Ouvrir le visuel du lieu</a><h3>Entrepôt East LS</h3></p>");
+    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/0QLg0zf9/gentrepot.png'><p><a href='https://i.postimg.cc/4NFGcMQR/IMG-20230108-043430.jpg' data-lightbox='image-4' data-title='Entrepôt Cypress'>Ouvrir le visuel du lieu</a><h3>Entrepôt East LS</h3></p>");
 L.marker([-6990, 3815.2], {icon: cardIcon35}).addTo(map)
-    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/0QLg0zf9/gentrepot.png'><p><a href='https://imgur.com/JOurhRf.png' data-lightbox='image-4' data-title='Entrepôt Walker & Sons'>Ouvrir le visuel du lieu</a><h3>Entrepôt South LS</h3></p>");
+    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/0QLg0zf9/gentrepot.png'><p><a href='https://i.postimg.cc/7Y7vC6h0/IMG-20230108-043652.jpg' data-lightbox='image-4' data-title='Entrepôt Walker & Sons'>Ouvrir le visuel du lieu</a><h3>Entrepôt South LS</h3></p>");
 L.marker([-7328, 3176], {icon: cardIcon35}).addTo(map)
-    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/0QLg0zf9/gentrepot.png'><p><a href='https://imgur.com/JOurhRf.png' data-lightbox='image-4' data-title='Entrepôt Bilgeco'>Ouvrir le visuel du lieu</a><h3>Entrepôt South LS</h3></p>");
+    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/0QLg0zf9/gentrepot.png'><p><a href='https://i.postimg.cc/HsLHVH5t/IMG-20230108-043437.jpg' data-lightbox='image-4' data-title='Entrepôt Bilgeco'>Ouvrir le visuel du lieu</a><h3>Entrepôt South LS</h3></p>");
 L.marker([-6555, 4389], {icon: cardIcon35}).addTo(map)
-    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/0QLg0zf9/gentrepot.png'><p><a href='https://imgur.com/JOurhRf.png' data-lightbox='image-4' data-title='Entrepôt Logistics Depot'>Ouvrir le visuel du lieu</a><h3>Entrepôt East LS</h3></p>");
+    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/0QLg0zf9/gentrepot.png'><p><a href='https://i.postimg.cc/BnGr20Py/IMG-20230108-043659.jpg' data-lightbox='image-4' data-title='Entrepôt Logistics Depot'>Ouvrir le visuel du lieu</a><h3>Entrepôt East LS</h3></p>");
 L.marker([-6951, 3041], {icon: cardIcon35}).addTo(map)
-    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/0QLg0zf9/gentrepot.png'><p><a href='https://imgur.com/JOurhRf.png' data-lightbox='image-4' data-title='Entrepôt Xero Gas Factory'>Ouvrir le visuel du lieu</a><h3>Entrepôt South LS</h3></p>");
+    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/0QLg0zf9/gentrepot.png'><p><a href='https://i.postimg.cc/ZYXtK3bJ/IMG-20230108-043706.jpg' data-lightbox='image-4' data-title='Entrepôt Xero Gas Factory'>Ouvrir le visuel du lieu</a><h3>Entrepôt South LS</h3></p>");
  L.marker([-5396, 3579.2], {icon: cardIcon35}).addTo(map)
-    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/0QLg0zf9/gentrepot.png'><p><a href='https://imgur.com/JOurhRf.png' data-lightbox='image-4' data-title='Entrepôt West Vinewood Backlot'>Ouvrir le visuel du lieu</a><h3>Entrepôt North LS</h3></p>");
+    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/0QLg0zf9/gentrepot.png'><p><a href='https://i.postimg.cc/FsbW5J1W/IMG-20230108-043948.jpg' data-lightbox='image-4' data-title='Entrepôt West Vinewood Backlot'>Ouvrir le visuel du lieu</a><h3>Entrepôt North LS</h3></p>");
 L.marker([-6955, 4422], {icon: cardIcon35}).addTo(map)
-    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/0QLg0zf9/gentrepot.png'><p><a href='https://imgur.com/JOurhRf.png' data-lightbox='image-4' data-title='Entrepôt Wholesale Furniture'>Ouvrir le visuel du lieu</a><h3>Entrepôt East LS</h3></p>");
+    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/0QLg0zf9/gentrepot.png'><p><a href='https://i.postimg.cc/dVqWqtyq/IMG-20230108-043938.jpg' data-lightbox='image-4' data-title='Entrepôt Wholesale Furniture'>Ouvrir le visuel du lieu</a><h3>Entrepôt East LS</h3></p>");
  
 L.marker([-5960, 4088], {icon: cardIcon35}).addTo(map)
-    .bindPopup("<h2>Moyen entrepôt</h2><img class='logo' src='https://i.postimg.cc/SxwPnny4/mentrepot.png'><p><a href='https://imgur.com/JOurhRf.png' data-lightbox='image-4' data-title='Entrepôt Railyard'>Ouvrir le visuel du lieu</a><h3>Entrepôt East LS</h3></p>");
+    .bindPopup("<h2>Moyen entrepôt</h2><img class='logo' src='https://i.postimg.cc/SxwPnny4/mentrepot.png'><p><a href='https://i.postimg.cc/90YGSZ1y/IMG-20230108-044201.jpg' data-lightbox='image-4' data-title='Entrepôt Railyard'>Ouvrir le visuel du lieu</a><h3>Entrepôt East LS</h3></p>");
 L.marker([-6808, 4116], {icon: cardIcon35}).addTo(map)
-    .bindPopup("<h2>Moyen entrepôt</h2><img class='logo' src='https://i.postimg.cc/SxwPnny4/mentrepot.png'><p><a href='https://imgur.com/JOurhRf.png' data-lightbox='image-4' data-title='Entrepôt Old Power Station'>Ouvrir le visuel du lieu</a><h3>Entrepôt South LS</h3></p>");
+    .bindPopup("<h2>Moyen entrepôt</h2><img class='logo' src='https://i.postimg.cc/SxwPnny4/mentrepot.png'><p><a href='https://i.postimg.cc/90YGSZ1y/IMG-20230108-044201.jpg' data-lightbox='image-4' data-title='Entrepôt Old Power Station'>Ouvrir le visuel du lieu</a><h3>Entrepôt South LS</h3></p>");
 L.marker([-7315, 3556.2], {icon: cardIcon35}).addTo(map)
-    .bindPopup("<h2>Moyen entrepôt</h2><img class='logo' src='https://i.postimg.cc/SxwPnny4/mentrepot.png'><p><a href='https://imgur.com/JOurhRf.png' data-lightbox='image-4' data-title='LS Marine Building 3'>Ouvrir le visuel du lieu</a><h3>Entrepôt South LS</h3></p>");
+    .bindPopup("<h2>Moyen entrepôt</h2><img class='logo' src='https://i.postimg.cc/SxwPnny4/mentrepot.png'><p><a href='https://i.postimg.cc/rFh4r8Hy/IMG-20230108-044336.jpg' data-lightbox='image-4' data-title='LS Marine Building 3'>Ouvrir le visuel du lieu</a><h3>Entrepôt South LS</h3></p>");
 L.marker([-6419, 3560], {icon: cardIcon35}).addTo(map)
-    .bindPopup("<h2>Moyen entrepôt</h2><img class='logo' src='https://i.postimg.cc/SxwPnny4/mentrepot.png'><p><a href='https://imgur.com/JOurhRf.png' data-lightbox='image-4' data-title='Entrepôt Disused Factory Outlet'>Ouvrir le visuel du lieu</a><h3>Entrepôt South LS</h3></p>");
+    .bindPopup("<h2>Moyen entrepôt</h2><img class='logo' src='https://i.postimg.cc/SxwPnny4/mentrepot.png'><p><a href='https://i.postimg.cc/JzGk3fwN/IMG-20230108-044330.jpg' data-lightbox='image-4' data-title='Entrepôt Disused Factory Outlet'>Ouvrir le visuel du lieu</a><h3>Entrepôt South LS</h3></p>");
 L.marker([-5313, 3989], {icon: cardIcon35}).addTo(map)
-    .bindPopup("<h2>Moyen entrepôt</h2><img class='logo' src='https://i.postimg.cc/SxwPnny4/mentrepot.png'><p><a href='https://imgur.com/JOurhRf.png' data-lightbox='image-4' data-title='Entrepôt Discount Retail Unit'>Ouvrir le visuel du lieu</a><h3>Entrepôt North LS</h3></p>"); 
+    .bindPopup("<h2>Moyen entrepôt</h2><img class='logo' src='https://i.postimg.cc/SxwPnny4/mentrepot.png'><p><a href='https://i.postimg.cc/VN9LXgN9/IMG-20230108-044548.jpg' data-lightbox='image-4' data-title='Entrepôt Discount Retail Unit'>Ouvrir le visuel du lieu</a><h3>Entrepôt North LS</h3></p>"); 
 L.marker([-6699, 3406], {icon: cardIcon35}).addTo(map)
-    .bindPopup("<h2>Moyen entrepôt</h2><img class='logo' src='https://i.postimg.cc/SxwPnny4/mentrepot.png'><p><a href='https://imgur.com/JOurhRf.png' data-lightbox='image-4' data-title='Entrepôt Fridgit Annexe'>Ouvrir le visuel du lieu</a><h3>Entrepôt West LS</h3></p>");
+    .bindPopup("<h2>Moyen entrepôt</h2><img class='logo' src='https://i.postimg.cc/SxwPnny4/mentrepot.png'><p><a href='https://i.postimg.cc/Z5dq54tM/IMG-20230108-044555.jpg' data-lightbox='image-4' data-title='Entrepôt Fridgit Annexe'>Ouvrir le visuel du lieu</a><h3>Entrepôt West LS</h3></p>");
  L.marker([-6066, 2920], {icon: cardIcon35}).addTo(map)
-    .bindPopup("<h2>Moyen entrepôt</h2><img class='logo' src='https://i.postimg.cc/SxwPnny4/mentrepot.png'><p><a href='https://imgur.com/JOurhRf.png' data-lightbox='image-4' data-title='Entrepôt Derriere Lingerie Backlot'>Ouvrir le visuel du lieu</a><h3>Entrepôt North LS</h3></p>");
+    .bindPopup("<h2>Moyen entrepôt</h2><img class='logo' src='https://i.postimg.cc/SxwPnny4/mentrepot.png'><p><a href='https://i.postimg.cc/g04bYTJ7/IMG-20230108-044725.jpg' data-lightbox='image-4' data-title='Entrepôt Derriere Lingerie Backlot'>Ouvrir le visuel du lieu</a><h3>Entrepôt North LS</h3></p>");
 L.marker([-6932, 4789], {icon: cardIcon35}).addTo(map)
-    .bindPopup("<h2>Moyen entrepôt</h2><img class='logo' src='https://i.postimg.cc/SxwPnny4/mentrepot.png'><p><a href='https://imgur.com/JOurhRf.png' data-lightbox='image-4' data-title='Entrepôt GEE'>Ouvrir le visuel du lieu</a><h3>Entrepôt East LS</h3></p>");
+    .bindPopup("<h2>Moyen entrepôt</h2><img class='logo' src='https://i.postimg.cc/SxwPnny4/mentrepot.png'><p><a href='https://i.postimg.cc/RFDrtJwQ/IMG-20230108-044718.jpg' data-lightbox='image-4' data-title='Entrepôt GEE'>Ouvrir le visuel du lieu</a><h3>Entrepôt East LS</h3></p>");
 
 
 L.marker([-5408, 3480], {icon: cardIcon35}).addTo(map)
-    .bindPopup("<h2>Petit entrepôt</h2><img class='logo' src='https://i.postimg.cc/cLKj1RGT/pentrepot.png'><p><a href='https://imgur.com/JOurhRf.png' data-lightbox='image-4' data-title='Entrepôt Foreclosed Garage'>Ouvrir le visuel du lieu</a><h3>Entrepôt North LS</h3></p>");
+    .bindPopup("<h2>Petit entrepôt</h2><img class='logo' src='https://i.postimg.cc/cLKj1RGT/pentrepot.png'><p><a href='https://i.postimg.cc/q7Z0sFT3/IMG-20230108-044930.jpg' data-lightbox='image-4' data-title='Entrepôt Foreclosed Garage'>Ouvrir le visuel du lieu</a><h3>Entrepôt North LS</h3></p>");
 L.marker([-7474, 3943], {icon: cardIcon35}).addTo(map)
-    .bindPopup("<h2>Petit entrepôt</h2><img class='logo' src='https://i.postimg.cc/cLKj1RGT/pentrepot.png'><p><a href='https://imgur.com/JOurhRf.png' data-lightbox='image-4' data-title='Entrepôt Pier 400 Utility Building'>Ouvrir le visuel du lieu</a><h3>Entrepôt South LS</h3></p>");
+    .bindPopup("<h2>Petit entrepôt</h2><img class='logo' src='https://i.postimg.cc/cLKj1RGT/pentrepot.png'><p><a href='https://i.postimg.cc/j5pb9JCq/IMG-20230108-044925.jpg' data-lightbox='image-4' data-title='Entrepôt Pier 400 Utility Building'>Ouvrir le visuel du lieu</a><h3>Entrepôt South LS</h3></p>");
 L.marker([-7217, 3792], {icon: cardIcon35}).addTo(map)
-    .bindPopup("<h2>Petit entrepôt</h2><img class='logo' src='https://i.postimg.cc/cLKj1RGT/pentrepot.png'><p><a href='https://imgur.com/JOurhRf.png' data-lightbox='image-4' data-title='Entrepôt Pacific Bait Storage'>Ouvrir le visuel du lieu</a><h3>Entrepôt South LS</h3></p>");
+    .bindPopup("<h2>Petit entrepôt</h2><img class='logo' src='https://i.postimg.cc/cLKj1RGT/pentrepot.png'><p><a href='https://i.postimg.cc/0jRTZRHb/IMG-20230108-045109.jpg' data-lightbox='image-4' data-title='Entrepôt Pacific Bait Storage'>Ouvrir le visuel du lieu</a><h3>Entrepôt South LS</h3></p>");
 L.marker([-6362, 3045], {icon: cardIcon35}).addTo(map)
-    .bindPopup("<h2>Petit entrepôt</h2><img class='logo' src='https://i.postimg.cc/cLKj1RGT/pentrepot.png'><p><a href='https://imgur.com/JOurhRf.png' data-lightbox='image-4' data-title='Entrepôt White Widow Garage'>Ouvrir le visuel du lieu</a><h3>Entrepôt West LS</h3></p>");
+    .bindPopup("<h2>Petit entrepôt</h2><img class='logo' src='https://i.postimg.cc/cLKj1RGT/pentrepot.png'><p><a href='https://i.postimg.cc/Vvq3VG34/IMG-20230108-045103.jpg' data-lightbox='image-4' data-title='Entrepôt White Widow Garage'>Ouvrir le visuel du lieu</a><h3>Entrepôt West LS</h3></p>");
 L.marker([-6204, 4348.5], {icon: cardIcon35}).addTo(map)
-    .bindPopup("<h2>Petit entrepôt</h2><img class='logo' src='https://i.postimg.cc/cLKj1RGT/pentrepot.png'><p><a href='https://imgur.com/JOurhRf.png' data-lightbox='image-4' data-title='Entrepôt Celltowa Unit'>Ouvrir le visuel du lieu</a><h3>Entrepôt East LS</h3></p>");
+    .bindPopup("<h2>Petit entrepôt</h2><img class='logo' src='https://i.postimg.cc/cLKj1RGT/pentrepot.png'><p><a href='https://i.postimg.cc/mDxpW33n/IMG-20230108-045232.jpg' data-lightbox='image-4' data-title='Entrepôt Celltowa Unit'>Ouvrir le visuel du lieu</a><h3>Entrepôt East LS</h3></p>");
 L.marker([-6814, 3919], {icon: cardIcon35}).addTo(map)
-    .bindPopup("<h2>Petit entrepôt</h2><img class='logo' src='https://i.postimg.cc/cLKj1RGT/pentrepot.png'><p><a href='https://imgur.com/JOurhRf.png' data-lightbox='image-4' data-title='Entrepôt Convenience Store Lockup'>Ouvrir le visuel du lieu</a><h3>Entrepôt South LS</h3></p>");
+    .bindPopup("<h2>Petit entrepôt</h2><img class='logo' src='https://i.postimg.cc/cLKj1RGT/pentrepot.png'><p><a href='https://i.postimg.cc/SxvtDbnk/IMG-20230108-045239.jpg' data-lightbox='image-4' data-title='Entrepôt Convenience Store Lockup'>Ouvrir le visuel du lieu</a><h3>Entrepôt South LS</h3></p>");
  
