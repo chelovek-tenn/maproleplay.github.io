510c5c3fe2cf1aa51e796e8580e646c94fff4e99
l-a-roleplay <121698621+l-a-roleplay@users.noreply.github.com>
2023-01-08 22:09:33 +0100

    Update markers.js

--- js/markers.js
+++ js/markers.js
@@ -1,13 +1,13 @@
 L.marker([-6201, 4395], {icon: cardIcon36}).addTo(map)
-    .bindPopup("<h2>Garage haut de gamme</h2><img class='logo' src='hhttps://i.postimg.cc/htWhrs9M/garagelogo.png'><p><a href='https://imgur.com/JOurhRf.png' data-lightbox='image-4' data-title='Garage 0120 Murrieta Heights'>Ouvrir le visuel du lieu</a><h3>Garage East LS</h3></p>");
+    .bindPopup("<h2>Garage haut de gamme</h2><img class='logo' src='https://i.postimg.cc/XNk8Gt46/garagelogo.png'><p><a href='https://i.postimg.cc/Bv2DW1vf/Murrieta-height1.jpg' data-lightbox='image-4' data-title='Garage 0120 Murrieta Heights'>Ouvrir le visuel du lieu</a><h3>Garage East LS</h3></p>");
 L.marker([-6135, 4292], {icon: cardIcon36}).addTo(map)
-    .bindPopup("<h2>Garage haut de gamme</h2><img class='logo' src='hhttps://i.postimg.cc/htWhrs9M/garagelogo.png'><p><a href='https://imgur.com/JOurhRf.png' data-lightbox='image-4' data-title='Garage Popular Street, Unit 2'>Ouvrir le visuel du lieu</a><h3>Garage East LS</h3></p>");
+    .bindPopup("<h2>Garage haut de gamme</h2><img class='logo' src='https://i.postimg.cc/XNk8Gt46/garagelogo.png'><p><a href='https://i.postimg.cc/QCdWW4Jj/Popular-street.jpg' data-lightbox='image-4' data-title='Garage Popular Street, Unit 2'>Ouvrir le visuel du lieu</a><h3>Garage East LS</h3></p>");
 L.marker([-6026, 4259], {icon: cardIcon36}).addTo(map)
-    .bindPopup("<h2>Garage haut de gamme</h2><img class='logo' src='hhttps://i.postimg.cc/htWhrs9M/garagelogo.png'><p><a href='https://imgur.com/JOurhRf.png' data-lightbox='image-4' data-title='Garage 331 Supply Street'>Ouvrir le visuel du lieu</a><h3>Garage East LS</h3></p>");
+    .bindPopup("<h2>Garage haut de gamme</h2><img class='logo' src='https://i.postimg.cc/XNk8Gt46/garagelogo.png'><p><a href='https://i.postimg.cc/Qd25nqhq/Supply-street.jpg' data-lightbox='image-4' data-title='Garage 331 Supply Street'>Ouvrir le visuel du lieu</a><h3>Garage East LS</h3></p>");
 L.marker([-6998, 3041], {icon: cardIcon36}).addTo(map)
-    .bindPopup("<h2>Garage haut de gamme</h2><img class='logo' src='hhttps://i.postimg.cc/htWhrs9M/garagelogo.png'><p><a href='https://imgur.com/JOurhRf.png' data-lightbox='image-4' data-title='Garage Greenwich Parkway, Unit 76'>Ouvrir le visuel du lieu</a><h3>Garage South LS</h3></p>");
+    .bindPopup("<h2>Garage haut de gamme</h2><img class='logo' src='https://i.postimg.cc/XNk8Gt46/garagelogo.png'><p><a href='https://i.postimg.cc/C1Y8wLZB/Greenwich-park.jpg' data-lightbox='image-4' data-title='Garage Greenwich Parkway, Unit 76'>Ouvrir le visuel du lieu</a><h3>Garage South LS</h3></p>");
 L.marker([-7100, 3314], {icon: cardIcon36}).addTo(map)
-    .bindPopup("<h2>Garage haut de gamme</h2><img class='logo' src='hhttps://i.postimg.cc/htWhrs9M/garagelogo.png'><p><a href='https://imgur.com/JOurhRf.png' data-lightbox='image-4' data-title='Garage 1337 Exceptionalists Way'>Ouvrir le visuel du lieu</a><h3>Garage South LS</h3></p>");
+    .bindPopup("<h2>Garage haut de gamme</h2><img class='logo' src='https://i.postimg.cc/XNk8Gt46/garagelogo.png'><p><a href='https://i.postimg.cc/sxHGPYT7/Exceptionalists-way.jpg' data-lightbox='image-4' data-title='Garage 1337 Exceptionalists Way'>Ouvrir le visuel du lieu</a><h3>Garage South LS</h3></p>");
 L.marker([-7110, 4433], {icon: cardIcon36}).addTo(map)
-    .bindPopup("<h2>Garage haut de gamme</h2><img class='logo' src='hhttps://i.postimg.cc/htWhrs9M/garagelogo.png'><p><a href='https://imgur.com/JOurhRf.png' data-lightbox='image-4' data-title='Garage 1623 South Shambles Street'>Ouvrir le visuel du lieu</a><h3>Garage East LS</h3></p>");
+    .bindPopup("<h2>Garage haut de gamme</h2><img class='logo' src='https://i.postimg.cc/XNk8Gt46/garagelogo.png'><p><a href='https://i.postimg.cc/sXchxdSn/Shambles-street.jpg' data-lightbox='image-4' data-title='Garage 1623 South Shambles Street'>Ouvrir le visuel du lieu</a><h3>Garage East LS</h3></p>");
 
