effff13dae7ee91fe4530b8d4c35e805a697209d
l-a-roleplay <121698621+l-a-roleplay@users.noreply.github.com>
2023-01-08 20:25:55 +0100

    Update markers.js

--- js/markers.js
+++ js/markers.js
@@ -1,2 +1,13 @@
-L.marker([-6120, 4258], {icon: cardIcon35}).addTo(map)
-    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/0QLg0zf9/gentrepot.png'><p><a data-lightbox='image-4' data-title='Entrepôt Darnel Bros'>Ouvrir le visuel du lieu</a><h3>Entrepôt East LS</h3></p>");
+L.marker([-6201, 4395], {icon: cardIcon36}).addTo(map)
+    .bindPopup("<h2>Garage haut de gamme</h2><img class='logo' src='hhttps://i.postimg.cc/htWhrs9M/garagelogo.png'><p><a href='https://imgur.com/JOurhRf.png' data-lightbox='image-4' data-title='Garage 0120 Murrieta Heights'>Ouvrir le visuel du lieu</a><h3>Garage East LS</h3></p>");
+L.marker([-6135, 4292], {icon: cardIcon36}).addTo(map)
+    .bindPopup("<h2>Garage haut de gamme</h2><img class='logo' src='hhttps://i.postimg.cc/htWhrs9M/garagelogo.png'><p><a href='https://imgur.com/JOurhRf.png' data-lightbox='image-4' data-title='Garage Popular Street, Unit 2'>Ouvrir le visuel du lieu</a><h3>Garage East LS</h3></p>");
+L.marker([-6026, 4259], {icon: cardIcon36}).addTo(map)
+    .bindPopup("<h2>Garage haut de gamme</h2><img class='logo' src='hhttps://i.postimg.cc/htWhrs9M/garagelogo.png'><p><a href='https://imgur.com/JOurhRf.png' data-lightbox='image-4' data-title='Garage 331 Supply Street'>Ouvrir le visuel du lieu</a><h3>Garage East LS</h3></p>");
+L.marker([-6998, 3041], {icon: cardIcon36}).addTo(map)
+    .bindPopup("<h2>Garage haut de gamme</h2><img class='logo' src='hhttps://i.postimg.cc/htWhrs9M/garagelogo.png'><p><a href='https://imgur.com/JOurhRf.png' data-lightbox='image-4' data-title='Garage Greenwich Parkway, Unit 76'>Ouvrir le visuel du lieu</a><h3>Garage South LS</h3></p>");
+L.marker([-7100, 3314], {icon: cardIcon36}).addTo(map)
+    .bindPopup("<h2>Garage haut de gamme</h2><img class='logo' src='hhttps://i.postimg.cc/htWhrs9M/garagelogo.png'><p><a href='https://imgur.com/JOurhRf.png' data-lightbox='image-4' data-title='Garage 1337 Exceptionalists Way'>Ouvrir le visuel du lieu</a><h3>Garage South LS</h3></p>");
+L.marker([-7110, 4433], {icon: cardIcon36}).addTo(map)
+    .bindPopup("<h2>Garage haut de gamme</h2><img class='logo' src='hhttps://i.postimg.cc/htWhrs9M/garagelogo.png'><p><a href='https://imgur.com/JOurhRf.png' data-lightbox='image-4' data-title='Garage 1623 South Shambles Street'>Ouvrir le visuel du lieu</a><h3>Garage East LS</h3></p>");
+
