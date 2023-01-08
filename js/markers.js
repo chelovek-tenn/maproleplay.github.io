4a6c97d61f4e4fbd4b3b36dd60504e0724c35274
l-a-roleplay <121698621+l-a-roleplay@users.noreply.github.com>
2023-01-08 23:37:09 +0100

    Update markers.js

--- js/markers.js
+++ js/markers.js
@@ -10,4 +10,23 @@ L.marker([-7100, 3314], {icon: cardIcon36}).addTo(map)
     .bindPopup("<h2>Garage haut de gamme</h2><img class='logo' src='https://i.postimg.cc/XNk8Gt46/garagelogo.png'><p><a href='https://i.postimg.cc/sxHGPYT7/Exceptionalists-way.jpg' data-lightbox='image-4' data-title='Garage 1337 Exceptionalists Way'>Ouvrir le visuel du lieu</a><h3>Garage South LS</h3></p>");
 L.marker([-7110, 4433], {icon: cardIcon36}).addTo(map)
     .bindPopup("<h2>Garage haut de gamme</h2><img class='logo' src='https://i.postimg.cc/XNk8Gt46/garagelogo.png'><p><a href='https://i.postimg.cc/sXchxdSn/Shambles-street.jpg' data-lightbox='image-4' data-title='Garage 1623 South Shambles Street'>Ouvrir le visuel du lieu</a><h3>Garage East LS</h3></p>");
+L.marker([-5347, 3570], {icon: cardIcon36}).addTo(map)
+    .bindPopup("<h2>Garage haut de gamme</h2><img class='logo' src='https://i.postimg.cc/XNk8Gt46/garagelogo.png'><p><a href='https://i.postimg.cc/fbVT44T4/Eclipse.jpg' data-lightbox='image-4' data-title='Garage Eclipse Blvd Garage'>Ouvrir le visuel du lieu</a><h3>Garage North LS</h3></p>");
+
+L.marker([-6515, 4086], {icon: cardIcon36}).addTo(map)
+    .bindPopup("<h2>Garage moyenne de gamme</h2><img class='logo' src='hhttps://i.postimg.cc/htWhrs9M/garagelogo.png'><p><a href='https://i.postimg.cc/7Yr9t7nD/Roy-lowenstein.jpg' data-lightbox='image-4' data-title='Garage 0552 Roy Lowenstein Boulevard'>Ouvrir le visuel du lieu</a><h3>Garage South LS</h3></p>");
+L.marker([-6116, 4340], {icon: cardIcon36}).addTo(map)
+    .bindPopup("<h2>Garage moyenne de gamme</h2><img class='logo' src='hhttps://i.postimg.cc/htWhrs9M/garagelogo.png'><p><a href='https://i.postimg.cc/C1fCmLZz/Popular-streetbis.jpg' data-lightbox='image-4' data-title='Garage Popular Street, Unit 14'>Ouvrir le visuel du lieu</a><h3>Garage East LS</h3></p>");
+L.marker([-6615, 3753], {icon: cardIcon36}).addTo(map)
+    .bindPopup("<h2>Garage moyenne de gamme</h2><img class='logo' src='hhttps://i.postimg.cc/htWhrs9M/garagelogo.png'><p><a href='https://i.postimg.cc/7LRM3TZF/Davis.jpg' data-lightbox='image-4' data-title='Garage 1905 Davis Avenue'>Ouvrir le visuel du lieu</a><h3>Garage South LS</h3></p>");
+L.marker([-6549, 4062], {icon: cardIcon36}).addTo(map)
+    .bindPopup("<h2>Garage moyenne de gamme</h2><img class='logo' src='hhttps://i.postimg.cc/htWhrs9M/garagelogo.png'><p><a href='https://i.postimg.cc/fbyfSvD7/Davis-0432.jpg' data-lightbox='image-4' data-title='Garage 0432 Davis Avenue'>Ouvrir le visuel du lieu</a><h3>Garage South LS</h3></p>");
+L.marker([-6287, 4314], {icon: cardIcon36}).addTo(map)
+    .bindPopup("<h2>Garage moyenne de gamme</h2><img class='logo' src='hhttps://i.postimg.cc/htWhrs9M/garagelogo.png'><p><a href='https://i.postimg.cc/3xnZwq5J/Olympic-freeway.jpg' data-lightbox='image-4' data-title='Garage Olympic Freeway, Unit 1'>Ouvrir le visuel du lieu</a><h3>Garage East LS</h3></p>");
+L.marker([-7000.75, 4328.5], {icon: cardIcon36}).addTo(map)
+    .bindPopup("<h2>Garage moyenne de gamme</h2><img class='logo' src='hhttps://i.postimg.cc/htWhrs9M/garagelogo.png'><p><a href='https://i.postimg.cc/Y2YfF0KP/Dry-dock.jpg' data-lightbox='image-4' data-title='Garage 4531 Dry Dock Street'>Ouvrir le visuel du lieu</a><h3>Garage East LS</h3></p>");
+L.marker([-3753, 3011], {icon: cardIcon36}).addTo(map)
+    .bindPopup("<h2>Garage moyenne de gamme</h2><img class='logo' src='hhttps://i.postimg.cc/htWhrs9M/garagelogo.png'><p><a href='https://i.postimg.cc/D0rr3jPh/Route-68.jpg' data-lightbox='image-4' data-title='Garage 8754 Route 68'>Ouvrir le visuel du lieu</a><h3>Garage Blaine County</h3></p>");
+L.marker([-3693, 3881], {icon: cardIcon36}).addTo(map)
+    .bindPopup("<h2>Garage moyenne de gamme</h2><img class='logo' src='hhttps://i.postimg.cc/htWhrs9M/garagelogo.png'><p><a href='https://i.postimg.cc/8cQR3QWw/Approach.jpg' data-lightbox='image-4' data-title='Garage 870 Route 68 Approach'>Ouvrir le visuel du lieu</a><h3>Garage Blaine County</h3></p>");
 
