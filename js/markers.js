1f2baf918d7e803eba092ebd0767301ff1e4f073
l-a-roleplay <121698621+l-a-roleplay@users.noreply.github.com>
2023-01-08 02:44:48 +0100

    Update markers.js

--- js/markers.js
+++ js/markers.js
@@ -1,8 +1,17 @@
-L.marker([-5662, 3401], {icon: cardIcon4}).addTo(map)
-    .bindPopup("<h2>Gouvernement</h2><img class='logo' src='https://i.postimg.cc/FKFth8y8/image-1.png'><p><a href='https://i.postimg.cc/Xv1P4J7B/IMG-20230102-024358.jpg' data-lightbox='image-4' data-title='Gouvernement'>Ouvrir le visuel du lieu</a><h3>Siège du gouvernement</h3></p>");
+L.marker([-6120, 4258], {icon: cardIcon35}).addTo(map)
+    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/0QLg0zf9/gentrepot.png' data-lightbox='image-4' data-title='Entrepôt Darnel Bros'>Ouvrir le visuel du lieu</a><h3>Entrepôt East LS</h3></p>");
+L.marker([-7188, 4420], {icon: cardIcon35}).addTo(map)
+    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/0QLg0zf9/gentrepot.png' data-lightbox='image-4' data-title='Entrepôt Cypress'>Ouvrir le visuel du lieu</a><h3>Entrepôt East LS</h3></p>");
+L.marker([-6990, 3815.2], {icon: cardIcon35}).addTo(map)
+    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/0QLg0zf9/gentrepot.png' data-lightbox='image-4' data-title='Entrepôt Walker & Sons'>Ouvrir le visuel du lieu</a><h3>Entrepôt South LS</h3></p>");
+L.marker([-7328, 3176], {icon: cardIcon35}).addTo(map)
+    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/0QLg0zf9/gentrepot.png' data-lightbox='image-4' data-title='Entrepôt Bilgeco'>Ouvrir le visuel du lieu</a><h3>Entrepôt South LS</h3></p>");
+L.marker([-6555, 4389], {icon: cardIcon35}).addTo(map)
+    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/0QLg0zf9/gentrepot.png' data-lightbox='image-4' data-title='Entrepôt Logistics Depot'>Ouvrir le visuel du lieu</a><h3>Entrepôt East LS</h3></p>");
+L.marker([-6951, 3041], {icon: cardIcon35}).addTo(map)
+    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/0QLg0zf9/gentrepot.png' data-lightbox='image-4' data-title='Entrepôt Xero Gas Factory'>Ouvrir le visuel du lieu</a><h3>Entrepôt South LS</h3></p>");
+ L.marker([-5396, 3579.2], {icon: cardIcon35}).addTo(map)
+    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/0QLg0zf9/gentrepot.png' data-lightbox='image-4' data-title='Entrepôt West Vinewood Backlot'>Ouvrir le visuel du lieu</a><h3>Entrepôt North LS</h3></p>");
+L.marker([-6955, 4422], {icon: cardIcon35}).addTo(map)
+    .bindPopup("<h2>Grand entrepôt</h2><img class='logo' src='https://i.postimg.cc/0QLg0zf9/gentrepot.png' data-lightbox='image-4' data-title='Entrepôt Wholesale Furniture'>Ouvrir le visuel du lieu</a><h3>Entrepôt East LS</h3></p>");
  
-L.marker([-5744.6, 3472], {icon: cardIcon3}).addTo(map)
-    .bindPopup("<h2>Hôpital</h2><img class='logo' src='https://i.postimg.cc/FsZRsgyd/image.png'><p><a href='https://i.postimg.cc/WpSLd9yv/neblife-fa8551fbc3a20dd3fd5c57cef430459e.png' data-lightbox='image-4' data-title='Hôpital'>Ouvrir le visuel du lieu</a><h3>Hôpital Mount Zonah</h3></p>");
-
-L.marker([-6558, 3981.2], {icon: cardIcon2}).addTo(map)
-    .bindPopup("<h2>Commissairiat</h2><img class='logo' src='https://i.postimg.cc/jS9mbctb/image.png'><p><a href='https://i.postimg.cc/xC1DfqRc/IMG-20230102-033753.jpg' data-lightbox='image-4' data-title='Commissairiat'>Ouvrir le visuel du lieu</a><h3>Poste de police South Los Santos</h3></p>");
