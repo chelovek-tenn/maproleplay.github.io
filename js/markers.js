aa073ad100da33f61f90d40af72435df23f61e68
chelovek-tenn <121698621+chelovek-tenn@users.noreply.github.com>
2023-04-04 14:47:03 +0200

    Update markers.js

--- js/markers.js
+++ js/markers.js
@@ -1,6 +1,8 @@
-map.on('click', function(e) {
-  var marker = L.marker(e.latlng, {icon: cardIcon1}).addTo(map);
-  var url = 'https://chelovek-tenn.github.io/maproleplay.github.io/agence.html#2/' + e.latlng.lat + '/' + e.latlng.lng;
-  marker.bindPopup('<a href="' + url + '">Lien vers mon site</a>').openPopup();
-  alert("Repère placé avec succès!");
-});
+var hash = window.location.hash.substring(1).split('/');
+var zoom = hash[0];
+var lat = hash[1];
+var lng = hash[2];
+
+// Initialiser la carte
+var map = L.map('map').setView([lat, lng], zoom);
+var marker = L.marker([lat, lng], {icon: cardIcon4}).addTo(map);
