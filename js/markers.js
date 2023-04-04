a4e1f0ca1823d4f91874568a2ce4193bd25f0caf
chelovek-tenn <121698621+chelovek-tenn@users.noreply.github.com>
2023-04-04 22:10:02 +0200

    Update markers.js

--- js/markers.js
+++ js/markers.js
@@ -1,7 +1,16 @@
-var hash = window.location.hash.substring(1).split('/');
+function initialize() {
+ var hash = window.location.hash.substring(1).split('/');
 var zoom = hash[0];
 var lat = hash[1];
 var lng = hash[2];
 
 // Initialiser la carte
 L.marker([lat, lng], {icon: cardIcon1}).addTo(map);
+}
+
+// appel de la fonction initialize au démarrage de la page
+window.onload = initialize;
+
+
+
+
