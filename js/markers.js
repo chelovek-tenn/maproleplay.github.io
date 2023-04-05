9cdefcf67be4ee37a7b311d84d152879ad8efaa6
chelovek-tenn <121698621+chelovek-tenn@users.noreply.github.com>
2023-04-06 01:53:34 +0200

    Update markers.js

--- js/markers.js
+++ js/markers.js
@@ -1,18 +1,24 @@
-var url = window.location.href;
-var match = url.match(/#(\d+)\/([\d\.]+)\/([\d\.]+)/);
+var markersLayer = new L.LayerGroup().addTo(map);
 
-if (match) {
-  var zoom = parseInt(match[1], 10);
-  var lat = parseFloat(match[2]);
-  var lng = parseFloat(match[3]);
+// Analyse de l'URL pour trouver les informations du marqueur
+var urlParams = new URLSearchParams(window.location.search);
+var markerParam = urlParams.get('marker');
 
-  if (!isNaN(zoom) && !isNaN(lat) && !isNaN(lng)) {
-    L.marker([lat, lng], {icon: cardMarker1}).addTo(map);
-  }
+if(markerParam){
+  // Création d'un marqueur à partir des coordonnées dans la chaîne de requête
+  var markerLatLng = markerParam.split(',');
+  var marker = L.marker([parseFloat(markerLatLng[0]), parseFloat(markerLatLng[1])], {icon: cardIcon1}).addTo(markersLayer);
 }
 
-
-
-
-
-
+// Configuration du plugin Leaflet-hash pour prendre en compte les marqueurs dans l'URL
+var hash = new L.Hash(map, { 
+  //ajout du marqueur à l'URL
+  updateCallback: function(){
+    var url = new URL(window.location.href);
+    if(markersLayer.getLayers().length > 0){
+      var markerLatLng = markersLayer.getLayers()[0].getLatLng();
+      url.searchParams.set('marker', markerLatLng.lat.toFixed(4)+','+markerLatLng.lng.toFixed(4));
+      window.history.replaceState({}, '', url);
+    }
+  }
+});
