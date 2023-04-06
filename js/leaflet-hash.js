cacd6a921fc91786716ab785bd0d02840c5f1366
chelovek-tenn <121698621+chelovek-tenn@users.noreply.github.com>
2023-04-06 03:24:28 +0200

    Update leaflet-hash.js

--- js/leaflet-hash.js
+++ js/leaflet-hash.js
@@ -17,8 +17,10 @@
 if (hashParams && hashParams.marker) {
     var markerCoords = hashParams.marker.split(',');
 
-	var numMarker=L.Icon.extend({options:{iconSize:[32,37],iconAnchor:[16,37]}}),cardIcon1=new numMarker({iconUrl:"markers/ammu.png"}),
-    L.marker([markerCoords[0], markerCoords[1], cardIcon1).addTo(map);
+    var numMarker = L.Icon.extend({ options: { iconSize:[32,37], iconAnchor:[16,37]}}),
+        cardIcon1 = new numMarker({iconUrl:"markers/ammu.png"});
+
+    L.marker([markerCoords[0], markerCoords[1]], {icon: cardIcon1}).addTo(map);
 }
             }
         }
