557aa80572365c3ea54137f0cc9a8c327c6f20c6
chelovek-tenn <121698621+chelovek-tenn@users.noreply.github.com>
2023-04-10 01:27:18 +0200

    Update core.js

--- js/core.js
+++ js/core.js
@@ -1,8 +1,11 @@
 var layer,mapExtent=[0,-8192,8192,0],mapMinZoom=1,mapMaxZoom=6,mapMaxResolution=.25,mapMinResolution=Math.pow(2,mapMaxZoom)*mapMaxResolution,tileExtent=[0,-8192,8192,0],crs=L.CRS.Simple;crs.transformation=new L.Transformation(1,-tileExtent[0],-1,tileExtent[3]),crs.scale=function(r){return Math.pow(2,r)/mapMinResolution},crs.zoom=function(r){return Math.log(r*mapMinResolution)/Math.LN2};var map=new L.Map("map",{maxZoom:mapMaxZoom,minZoom:mapMinZoom,crs:crs});layer=L.tileLayer("{z}/{x}/{y}.png",{minZoom:mapMinZoom,maxZoom:mapMaxZoom,tileSize:512,attribution:'Dispatcher Bot',noWrap:!0,tms:!1}).addTo(map),map.fitBounds([crs.unproject(L.point(mapExtent[2],mapExtent[3])),crs.unproject(L.point(mapExtent[0],mapExtent[1]))]),L.control.mousePosition().addTo(map);var numMarker=L.Icon.extend({options:{iconSize:[32,37],iconAnchor:[16,37]}}),cardIcon1=new numMarker({iconUrl:"markers/ammu.png"});var hash = new L.Hash(map);
 
-var markerParam = new URLSearchParams(window.location.hash.slice(1)).get('marker');
-if (markerParam) {
-  var markerCoords = markerParam.split(',');
-  var latlng = L.latLng(markerCoords[0], markerCoords[1]);
-  var marker = L.marker(latlng, {icon: cardIcon1}).addTo(map);
+var params = getParams();
+var mapmarkerParam = params.mapmarker;
+
+// Ajout du marqueur sur la carte si le paramètre est présent
+if (mapmarkerParam) {
+  var coords = mapmarkerParam.split(",");
+  var latlng = L.latLng(parseFloat(coords[0]), parseFloat(coords[1]));
+  L.marker(latlng).addTo(map);
 }
