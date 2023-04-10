5fc1118aa90d878d7c13c64fa6d9545cb3c6c869
chelovek-tenn <121698621+chelovek-tenn@users.noreply.github.com>
2023-04-10 12:07:19 +0200

    Update core.js

--- js/core.js
+++ js/core.js
@@ -1,21 +1,14 @@
-var layer,mapExtent=[0,-8192,8192,0],mapMinZoom=1,mapMaxZoom=6,mapMaxResolution=.25,mapMinResolution=Math.pow(2,mapMaxZoom)*mapMaxResolution,tileExtent=[0,-8192,8192,0],crs=L.CRS.Simple;crs.transformation=new L.Transformation(1,-tileExtent[0],-1,tileExtent[3]),crs.scale=function(r){return Math.pow(2,r)/mapMinResolution},crs.zoom=function(r){return Math.log(r*mapMinResolution)/Math.LN2};var map=new L.Map("map",{maxZoom:mapMaxZoom,minZoom:mapMinZoom,crs:crs});layer=L.tileLayer("{z}/{x}/{y}.png",{minZoom:mapMinZoom,maxZoom:mapMaxZoom,tileSize:512,attribution:'Dispatcher Bot',noWrap:!0,tms:!1}).addTo(map),map.fitBounds([crs.unproject(L.point(mapExtent[2],mapExtent[3])),crs.unproject(L.point(mapExtent[0],mapExtent[1]))]),L.control.mousePosition().addTo(map);var numMarker=L.Icon.extend({options:{iconSize:[32,37],iconAnchor:[16,37]}}),cardIcon1=new numMarker({iconUrl:"markers/ammu.png"});var hash = new L.Hash(map);
+var layer,mapExtent=[0,-8192,8192,0],mapMinZoom=1,mapMaxZoom=6,mapMaxResolution=.25,mapMinResolution=Math.pow(2,mapMaxZoom)*mapMaxResolution,tileExtent=[0,-8192,8192,0],crs=L.CRS.Simple;crs.transformation=new L.Transformation(1,-tileExtent[0],-1,tileExtent[3]),crs.scale=function(r){return Math.pow(2,r)/mapMinResolution},crs.zoom=function(r){return Math.log(r*mapMinResolution)/Math.LN2};var map=new L.Map("map",{maxZoom:mapMaxZoom,minZoom:mapMinZoom,crs:crs});layer=L.tileLayer("{z}/{x}/{y}.png",{minZoom:mapMinZoom,maxZoom:mapMaxZoom,tileSize:512,attribution:'Dispatcher Bot',noWrap:!0,tms:!1}).addTo(map),map.fitBounds([crs.unproject(L.point(mapExtent[2],mapExtent[3])),crs.unproject(L.point(mapExtent[0],mapExtent[1]))]),L.control.mousePosition().addTo(map);var numMarker=L.Icon.extend({options:{iconSize:[32,37],iconAnchor:[16,37]}}),cardIcon1=new numMarker({iconUrl:"markers/ammu.png"});
+var urlParams = new URLSearchParams(window.location.hash.slice(1));
 
-function getParams() {
-  var hash = window.location.hash.slice(1);
-  var params = {};
+// récupération des coordonnées du marqueur
+var markerCoords = urlParams.get('marker');
 
-  hash.split('&').forEach(function(item) {
-    var s = item.split('=');
-    params[s[0]] = s[1];
-  });
-
-  return params;
+// si les coordonnées du marqueur sont présentes, on les utilise pour créer le marqueur
+if (markerCoords) {
+  var latLng = markerCoords.split(',');
+  var marker = L.marker([latLng[0], latLng[1]], { icon: cardIcon1 }).addTo(map);
 }
+var hash = new L.Hash(map);
 
-var params = getParams();
-if (params.marker) {
-  var markerCoords = params.marker.split(',');
-  var lat = markerCoords[0];
-  var lng = markerCoords[1];
-  L.marker([lat, lng]).addTo(map);
-}
+
