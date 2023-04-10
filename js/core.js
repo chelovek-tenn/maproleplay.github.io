7f63e8197a708f578a513665700168e1bad43767
chelovek-tenn <121698621+chelovek-tenn@users.noreply.github.com>
2023-04-10 12:55:45 +0200

    Update core.js

--- js/core.js
+++ js/core.js
@@ -1,12 +1,17 @@
 var layer,mapExtent=[0,-8192,8192,0],mapMinZoom=1,mapMaxZoom=6,mapMaxResolution=.25,mapMinResolution=Math.pow(2,mapMaxZoom)*mapMaxResolution,tileExtent=[0,-8192,8192,0],crs=L.CRS.Simple;crs.transformation=new L.Transformation(1,-tileExtent[0],-1,tileExtent[3]),crs.scale=function(r){return Math.pow(2,r)/mapMinResolution},crs.zoom=function(r){return Math.log(r*mapMinResolution)/Math.LN2};var map=new L.Map("map",{maxZoom:mapMaxZoom,minZoom:mapMinZoom,crs:crs});layer=L.tileLayer("{z}/{x}/{y}.png",{minZoom:mapMinZoom,maxZoom:mapMaxZoom,tileSize:512,attribution:'Dispatcher Bot',noWrap:!0,tms:!1}).addTo(map),map.fitBounds([crs.unproject(L.point(mapExtent[2],mapExtent[3])),crs.unproject(L.point(mapExtent[0],mapExtent[1]))]),L.control.mousePosition().addTo(map);var numMarker=L.Icon.extend({options:{iconSize:[32,37],iconAnchor:[16,37]}}),cardIcon1=new numMarker({iconUrl:"markers/ammu.png"});var hash = new L.Hash(map);
 const urlParams = new URLSearchParams(window.location.search);
-const markerParam = urlParams.get('marker');
 
-// si le paramètre marker est présent, ajouter un marqueur à la carte
-if (markerParam) {
-  const [lat, lng] = markerParam.split(',');
-  const marker = L.marker([lat, lng]).addTo(map);
+// Vérification de la présence du paramètre "marker" dans l'URL
+if (urlParams.has('marker')) {
+  // Récupération des coordonnées du marqueur depuis le paramètre "marker"
+  const markerCoords = urlParams.get('marker').split(',');
+  const markerLat = parseFloat(markerCoords[0]);
+  const markerLng = parseFloat(markerCoords[1]);
 
-  // Centrage de la carte sur le marqueur
-  //map.setView(markerCoords, 13);
+  // Création d'un marqueur à partir des coordonnées récupérées
+  const marker = L.marker([markerLat, markerLng]).addTo(map);
+
+  // Ajustement de la vue de la carte pour afficher le marqueur
+  map.setView([markerLat, markerLng], 6);
 }
+
