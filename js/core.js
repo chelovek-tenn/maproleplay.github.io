d05ed10f6f16d650a8e3c7c5cae902c74c21466f
chelovek-tenn <121698621+chelovek-tenn@users.noreply.github.com>
2023-04-10 16:59:55 +0200

    Update core.js

--- js/core.js
+++ js/core.js
@@ -19,3 +19,4 @@ var hash = new L.Hash(map);
 if (window.location.href.indexOf('?marker=') !== -1) {
   var markerCoord = window.location.href.split('?marker=')[1].split(',');
   var marker = L.marker([markerCoord[0], markerCoord[1]]).addTo(map);
+}
