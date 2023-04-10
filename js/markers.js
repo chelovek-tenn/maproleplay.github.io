ae18c74750514cb1af0f484d643571c43f639016
chelovek-tenn <121698621+chelovek-tenn@users.noreply.github.com>
2023-04-10 15:05:47 +0200

    Update markers.js

--- js/markers.js
+++ js/markers.js
@@ -1,13 +1,1 @@
-const params = new URLSearchParams(window.location.hash.slice(1));
-const marker = params.get("marker");
 
-if (marker) {
-  const [markerlat, markerlng] = marker.split(",");
-
-  // Création d'un marqueur à partir des coordonnées récupérées
-L.marker([markerLat, markerLng], {icon: cardIcon1}).addTo(map);
-
-  // Ajustement de la vue de la carte pour afficher le marqueur
-} else {
-console.log('Marker non detecter')
-}
